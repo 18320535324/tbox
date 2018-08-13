020cb302832d40c325299b77cbcd7cb5f099f371
saltedfish <m18320535324@163.com>
2018-08-13 20:36:26 +0800

    常量定义

---
+++ tox-auto-core/src/main/java/com/rdjz/models/PathConstant.java
@@ -0,0 +1,41 @@
+package com.rdjz.models;
+
+public class PathConstant {
+	public final static String templatePath = "/tox-auto-core/src/main/resources/generator/demo";
+	public final static String PACKAGE_PREFIX = "/src/main/java";
+	
+	
+	public static String mapperProjectPath = "/tbox-base";
+	public static String mapperPackage = "/com/jjfinder/tbox/base/dao/mapper";
+
+	public static String modelProjectPath = "/tbox-common";
+	public static String modelPackage = "/com/jjfinder/tbox/common/model";
+
+	public static String serviceProjectPath = "/tbox-base";
+	public static String servicePackage = "/com/jjfinder/tbox/base/service";
+
+	public static String serviceImplProjectPath = "/tbox-base";
+	public static String serviceImplPackage = "/com/jjfinder/tbox/base/service/impl";
+
+	public static String controllerProjectPath = "/tbox-core";
+	public static String controllerPackage = "/com/jjfinder/tbox/core/controller";
+
+	public static String apiUrlProjectPath = "/tbox-common";
+	public static String apiUrlPackage = "/com/jjfinder/tbox/common/constant";
+	
+	
+	/**
+	 * 默认的微服务结构
+	 */
+	public static String mapperFinalPath;
+	
+	public static String MODEL_FINAL_PATH;
+	
+	public static String serviceFinalPath;
+	
+	public static String serviceImplFinalPath;
+	
+	public static String controllerFinalPath;
+	
+	public static String apiUrl;
+}
