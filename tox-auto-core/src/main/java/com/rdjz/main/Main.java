5fa8389255a44b1202f7060b33c95f031c6152ae
saltedfish <m18320535324@163.com>
2018-08-13 20:39:07 +0800

    隐藏参数

--- tox-auto-core/src/main/java/com/rdjz/main/Main.java
+++ tox-auto-core/src/main/java/com/rdjz/main/Main.java
@@ -4,14 +4,17 @@ import static com.rdjz.common.db.utils.Util.convertSlashPath;
 
 import com.rdjz.common.db.utils.DatabaseMetaDateApplication;
 import com.rdjz.common.db.utils.Util;
+import com.rdjz.models.PathConstant;
 /**
  * 
  * @author salted fish
  * @Date 2018年8月13日
- * 修改projectPath路径为项目路径
- * 修改
+ * projectPath指定生成代码的路径
+ * 指定templateProjectPath为当前项目位置
+ * tableName为表名
+ * modelName为Bean的名称
  */
-public class Main {
+public class Main extends PathConstant{
 	public final static String jdbcDriver = "com.mysql.jdbc.Driver";
 	public final static String jdbcUrl = "jdbc:postgresql://127.0.0.1/tbox-dev";
 	public final static String jdbcUser = "postgres";
@@ -24,45 +27,9 @@ public class Main {
 	/**
 	 * 默认微服务结构，这个项目路径要直接配置
 	 */
-	public final static String projectPath = "D:/saltedfish/work/git/repository-nf3-01/TBox";
-	public final static String templateProjectPath = "D:/saltedfish/work/workspace/workspace-saltedfish-01";
-	public final static String templatePath = "/tox-auto-core/src/main/resources/generator/demo";
-	public final static String PACKAGE_PREFIX = "/src/main/java";
+	public final static String projectPath = "D:/saltedfish/work/git/repository-nf3-02/TBox";
+	public final static String templateProjectPath = "D:/saltedfish/work/git/repository-saltedfish-02/temp--01/tbox";
 	
-	
-	public static String mapperProjectPath = "/tbox-base";
-	public static String mapperPackage = "/com/jjfinder/tbox/base/dao/mapper";
-
-	public static String modelProjectPath = "/tbox-common";
-	public static String modelPackage = "/com/jjfinder/tbox/common/model";
-
-	public static String serviceProjectPath = "/tbox-base";
-	public static String servicePackage = "/com/jjfinder/tbox/base/service";
-
-	public static String serviceImplProjectPath = "/tbox-base";
-	public static String serviceImplPackage = "/com/jjfinder/tbox/base/service/impl";
-
-	public static String controllerProjectPath = "/tbox-core";
-	public static String controllerPackage = "/com/jjfinder/tbox/core/controller";
-
-	public static String apiUrlProjectPath = "/tbox-common";
-	public static String apiUrlPackage = "/com/jjfinder/tbox/common/constant";
-	
-	
-	/**
-	 * 默认的微服务结构
-	 */
-	public static String mapperFinalPath;
-	
-	public static String MODEL_FINAL_PATH;
-	
-	public static String serviceFinalPath;
-	
-	public static String serviceImplFinalPath;
-	
-	public static String controllerFinalPath;
-	
-	public static String apiUrl;
 	public static void main(String[] args) throws Exception {
 
 		Main.initPath();
